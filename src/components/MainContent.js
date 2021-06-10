4426f4854ea928e3a067e075b01526763a8dcfff
André Aloisio Mendes <andre.mendes@serpro.gov.br>
2021-06-10 20:20:14 -0300

    Include text fields

--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -8,9 +8,16 @@ import {
 } from '@material-ui/pickers';
 import DateFnsUtils from '@date-io/date-fns';
 import React from 'react';
+import Switch from '@material-ui/core/Switch';
+import TextField  from '@material-ui/core/TextField';
 
 const MainContent = () => {
   const [selectedDate, handleDateChange] = React.useState(new Date());
+  const handleSwitchChange = (event) => {
+    setTimeout(() => {
+      alert('Switch value is ' + event.target.checked);
+    }, 100);
+  }
   return (
     <div className={styles['main-content']}>
       <h3>Buttons</h3>
@@ -26,6 +33,15 @@ const MainContent = () => {
         <TimePicker value={selectedDate} onChange={handleDateChange} />
         <DateTimePicker value={selectedDate} onChange={handleDateChange} />
       </MuiPickersUtilsProvider>
+      <h3>Switch</h3>
+      <Switch
+        onChange={handleSwitchChange}
+        name="checkedA"
+        inputProps={{ 'aria-label': 'secondary checkbox' }}
+      />
+      <h3>Text Field</h3>
+      <TextField id="outlined-basic" label="Name" placeholder="My Name"/>
+      <TextField id="outlined-basic" label="E-mail" placeholder="E-mail" style={{marginLeft: 20}}/>
     </div>
   );
 };
